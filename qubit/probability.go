d97ef8e34501873586d0be98dd29f7b54151192b
Ben Luxford <me@benluxford.com>
2019-02-24 22:42:03 +1100

    added max to probibility

--- qubit/probability.go
+++ qubit/probability.go
@@ -1,1 +1,34 @@
 package qubit
+
+import "math"
+
+// Max : Return the max probability
+func Max(input []float64) (max float64) {
+	// set max to the first value
+	max = input[0]
+	// for all numbers in input slice
+	for _, number := range input {
+		// return the max of the two numbers and set max to highest
+		max = math.Max(max, number)
+	}
+	// return the highest number
+	return
+}
+
+// func Min(p []float64) float64 {
+// 	min := p[0]
+// 	for _, pp := range p {
+// 		min = math.Max(min, pp)
+// 	}
+
+// 	return min
+// }
+
+// func Sum(p []float64) float64 {
+// 	var sum float64
+// 	for _, pp := range p {
+// 		sum = sum + pp
+// 	}
+
+// 	return sum
+// }
