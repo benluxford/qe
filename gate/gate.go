85ba82359c88b243c3bbba5c8449e399fcfad0bf
Ben Luxford <me@benluxford.com>
2019-02-24 23:06:01 +1100

    added y gate

--- gate/gate.go
+++ gate/gate.go
@@ -52,12 +52,12 @@ func X(bit ...int) matrix.Matrix {
 	return matrix.TensorProductN(m, bit...)
 }
 
-// func Y(bit ...int) matrix.Matrix {
-// 	m := make(matrix.Matrix, 2)
-// 	m[0] = []complex128{0, -1i}
-// 	m[1] = []complex128{1i, 0}
-// 	return matrix.TensorProductN(m, bit...)
-// }
+func Y(bit ...int) matrix.Matrix {
+	m := make(matrix.Matrix, 2)
+	m[0] = []complex128{0, -1i}
+	m[1] = []complex128{1i, 0}
+	return matrix.TensorProductN(m, bit...)
+}
 
 // func Z(bit ...int) matrix.Matrix {
 // 	m := make(matrix.Matrix, 2)
